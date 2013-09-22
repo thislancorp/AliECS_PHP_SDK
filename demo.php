f34b88a52a11fb3f8f0c91e75830c48e9daf9bea
thislancorp <technical@thislancorp.com>
2013-09-22 17:05:51 +0800

    Delete demo.php

--- demo.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "ecs.sdk.class.php";
-
-$accessKeyId = '';
-$accessKeySecret = '';
-$ecs=new ECS($accessKeyId,$accessKeySecret);
-print_r($ecs->describeInstanceAttribute('AY120723082752d383204'));
-print_r($ecs->describeInstanceTypes());
-?>
